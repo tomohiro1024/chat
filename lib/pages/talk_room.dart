93ae142c690eefa153726a7af859230340684378
tomohiro1024 <gongyezhihong732@gmail.com>
2022-01-08 16:25:29 +0900

    入力ボックスのUI作成

--- lib/pages/talk_room.dart
+++ lib/pages/talk_room.dart
@@ -15,7 +15,8 @@ class TalkRoom extends StatefulWidget {
 class _TalkRoomState extends State<TalkRoom> {
   List<Message> messageList = [
     Message('Hello', true, DateTime(2022, 1, 1, 10, 20)),
-    Message('안녕하세요', false, DateTime(2022, 1, 1, 11, 15)),
+    Message('안녕하세요ああああああああああああいいいいいいいいいいいいいいいいいいいいいいい', false,
+        DateTime(2022, 1, 1, 11, 15)),
   ];
   @override
   Widget build(BuildContext context) {
@@ -25,24 +26,90 @@ class _TalkRoomState extends State<TalkRoom> {
         backgroundColor: Colors.orange,
         title: Text(widget.name, style: TextStyle(color: Colors.black)),
       ),
-      body: ListView.builder(
-          itemCount: messageList.length,
-          itemBuilder: (context, index) {
-            return Row(
-              textDirection: messageList[index].isMe
-                  ? TextDirection.ltr
-                  : TextDirection.rtl,
-              children: [
-                Container(
-                    color: messageList[index].isMe
-                        ? Colors.greenAccent
-                        : Colors.yellow,
-                    child: Text(messageList[index].message)),
-                Text(intl.DateFormat('HH:mm')
-                    .format(messageList[index].sendTime)),
-              ],
-            );
-          }),
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 50.0),
+            child: ListView.builder(
+                // お互いのコメントが画面幅を超過した場合、スクロールできるようにする。
+                physics: RangeMaintainingScrollPhysics(),
+                shrinkWrap: true,
+                reverse: true,
+                itemCount: messageList.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      textDirection: messageList[index].isMe
+                          ? TextDirection.ltr
+                          : TextDirection.rtl,
+                      children: [
+                        Container(
+                            // コメントの幅を6割程度にする。（コメントが長すぎたら改行する）
+                            constraints: BoxConstraints(
+                                maxWidth:
+                                    MediaQuery.of(context).size.width * 0.6),
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 10.0, vertical: 6.0),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              color: messageList[index].isMe
+                                  ? Colors.greenAccent
+                                  : Colors.yellow,
+                            ),
+                            child: Text(messageList[index].message)),
+                        Opacity(
+                          opacity: 0.6,
+                          child: Text(
+                            intl.DateFormat('HH:mm')
+                                .format(messageList[index].sendTime),
+                            style:
+                                TextStyle(fontSize: 11, color: Colors.orange),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                }),
+          ),
+          // 入力ボックスのUI
+          Align(
+            // 入力ボックスを画面の下に配置する
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 50,
+              color: Colors.orange,
+              child: Row(
+                children: [
+                  Expanded(
+                      child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                  )),
+                  IconButton(
+                    onPressed: () {
+                      print('send');
+                    },
+                    icon: Icon(
+                      Icons.arrow_upward,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
