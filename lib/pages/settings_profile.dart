dc502bf918c892c815632d878051c1ab19168439
tomohiro1024 <gongyezhihong732@gmail.com>
2022-02-11 15:24:54 +0900

    一旦、完成とする

--- lib/pages/settings_profile.dart
+++ lib/pages/settings_profile.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:chat_app/model/user.dart';
 import 'package:chat_app/utils/firebase.dart';
+import 'package:chat_app/utils/shared_prefs.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
@@ -21,6 +22,18 @@ class _SettingsProfilePageState extends State<SettingsProfilePage> {
   String? imagePath;
   // 送信するテキストに何かしらの値が入ってきた場合の処理の管理
   TextEditingController controller = TextEditingController();
+
+  //0206
+  User? myProfile;
+  //0206
+  Future<User?> getMyImage() async {
+    String? myUid = SharedPrefs.getUid();
+    myProfile = await Firestore.getProfile(myUid!);
+  }
+
+  bool isUpdate() {
+    return myProfile != null;
+  }
 
   Future<void>? getImageFromGallery() async {
     // スマホの写真フォルダから写真を選択する
@@ -55,106 +68,121 @@ class _SettingsProfilePageState extends State<SettingsProfilePage> {
         title: Text('プロフィール編集'),
         backgroundColor: Colors.black,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Container(width: 90, child: Text('Name')),
-                Container(
-                  child: Expanded(
-                      child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      height: 36,
-                      child: TextField(
-                        controller: controller,
-                        decoration: InputDecoration(
-                          hintText: '名前を入力して下さい',
-                          hintStyle: TextStyle(
-                            color: Colors.grey[500],
-                            fontSize: 12,
+      body: FutureBuilder(
+          future: getMyImage(),
+          builder: (context, snapshot) {
+            return Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Container(width: 90, child: Text('Name')),
+                      Container(
+                        child: Expanded(
+                            child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            height: 36,
+                            child: TextField(
+                              controller: controller,
+                              decoration: InputDecoration(
+                                hintText: '${myProfile!.name}',
+                                hintStyle: TextStyle(
+                                  color: Colors.grey[500],
+                                  fontSize: 12,
+                                ),
+                                filled: true,
+                                fillColor: Colors.white70,
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                            ),
                           ),
-                          filled: true,
-                          fillColor: Colors.white70,
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Colors.black,
+                        )),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  // 画像選択UI
+                  Row(
+                    children: [
+                      Container(width: 100, child: Text('Thumbnail')),
+                      Expanded(
+                        child: Container(
+                          alignment: Alignment.topLeft,
+                          child: Container(
+                            width: 110,
+                            height: 35,
+                            child: ElevatedButton(
+                              onPressed: () async {
+                                await getImageFromGallery();
+                              },
+                              child: Text('画像を選択'),
+                              style: ElevatedButton.styleFrom(
+                                  primary: Colors.black),
                             ),
                           ),
                         ),
-                      ),
-                    ),
-                  )),
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
-            // 画像選択UI
-            Row(
-              children: [
-                Container(width: 100, child: Text('Thumbnail')),
-                Expanded(
-                  child: Container(
-                    alignment: Alignment.topLeft,
-                    child: Container(
-                      width: 110,
-                      height: 35,
-                      child: ElevatedButton(
-                        onPressed: () async {
-                          await getImageFromGallery();
-                        },
-                        child: Text('画像を選択'),
-                        style: ElevatedButton.styleFrom(primary: Colors.black),
-                      ),
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  // ここに自分の画像を表示させたい
+                  Container(
+                    child: CircleAvatar(
+                      backgroundImage: NetworkImage(myProfile!.imagePath),
                     ),
                   ),
-                )
-              ],
-            ),
-            SizedBox(height: 20),
-            // 画像が選択されていなかった場合、何も表示しない
-            image == null
-                ? Container()
-                : Container(
-                    width: 100,
-                    height: 100,
-                    child: Image.file(
-                      image!,
-                      fit: BoxFit.fill,
+                  image == null
+                      ? Container()
+                      : Container(
+                          width: 100,
+                          height: 100,
+                          child: Image.file(
+                            image!,
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.black, //ボタンの背景色
                     ),
-                  ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.black, //ボタンの背景色
-              ),
-              onPressed: () async {
-                //ここで画像アップロード処理
-                User newProfile = User(controller.text, '', imagePath!);
-                await Firestore.updeteProfile(newProfile);
-                print(controller.text);
+                    onPressed: isUpdate()
+                        ? () async {
+                            //ここで画像アップロード処理
+                            User newProfile =
+                                User(controller.text, '', imagePath!);
+                            await Firestore.updeteProfile(newProfile);
+                            print(controller.text);
 
-                if (controller.text == '') {
-                  final snackBar = SnackBar(
-                    backgroundColor: Colors.red,
-                    content: Text('名前もしくは画像を選択してください'),
-                  );
-                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                } else {
-                  final snackBar = SnackBar(
-                    backgroundColor: Colors.green,
-                    content: Text('編集しました'),
-                  );
-                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                }
-              },
-              child: Text('Edit'),
-            )
-          ],
-        ),
-      ),
+                            if (controller.text == '') {
+                              final snackBar = SnackBar(
+                                backgroundColor: Colors.red,
+                                content: Text('名前もしくは画像を選択してください'),
+                              );
+                              ScaffoldMessenger.of(context)
+                                  .showSnackBar(snackBar);
+                            } else {
+                              final snackBar = SnackBar(
+                                backgroundColor: Colors.green,
+                                content: Text('編集しました'),
+                              );
+                              ScaffoldMessenger.of(context)
+                                  .showSnackBar(snackBar);
+                            }
+                          }
+                        : null,
+                    child: Text('Edit'),
+                  )
+                ],
+              ),
+            );
+          }),
     );
   }
 }
